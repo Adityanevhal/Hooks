b59ab3d3ffcb8c08f64cb4de1156cfcb161cf0d8
Aditya Nevhal <tanmaykorate6@gmail.com>
2026-03-15 17:15:09 +0530

    ups

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,15 @@ const App = () => {
      React will preserve this state between re-renders. 
     useState returns a pair: the current state value and a function that lets you update it. */}
     const [num, setNum] = useState(0)
+    const [user, setUser] = useState("Adityav Nevhal")
      function ChangeNum(){
       setNum(200)
+      setUser("Arnav Nevhal")
      }
   return (
     <div>
     
-    <h1>Value of num is {num}</h1>
+    <h1>Value of num is {num} <br/> Name of user is {user}</h1>
 
     <button onClick={ChangeNum} >Click me!</button>
 
