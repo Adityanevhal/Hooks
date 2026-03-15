00b3cbcfed977e8ab1680a36efee89c3f885372e
Aditya Nevhal <tanmaykorate6@gmail.com>
2026-03-15 17:11:36 +0530

    pushed

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+
+const App = () => {
+
+  {/* useState is a Hook that allows you to have state variables in functional components. 
+    You can call it inside a function component to add some local state to it.
+     React will preserve this state between re-renders. 
+    useState returns a pair: the current state value and a function that lets you update it. */}
+    const [num, setNum] = useState(0)
+     function ChangeNum(){
+      setNum(200)
+     }
+  return (
+    <div>
+    
+    <h1>Value of num is {num}</h1>
+
+    <button onClick={ChangeNum} >Click me!</button>
+
+    </div>
+  )
+}
+
+export default App
